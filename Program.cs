850bdaee86f5476758859bc49dc855dde2c57d65
Gideon Idoko <iamgideonidoko@gmail.com>
2022-09-30 02:57:49 +0100

    Refactor with new object creation syntax

--- Program.cs
+++ Program.cs
@@ -12,31 +12,8 @@ namespace TablePDF
     internal class Program 
     {
         public static string output_path = "./output/pdf/";
-        public static string DateTimeToString(foxit.common.DateTime datetime)
-        {
-            string s_datetime = string.Format("{0}/{1}/{2}-{3}:{4}:{5} {6}{7}:{8}", datetime.year, datetime.month, datetime.day,
-                datetime.hour, datetime.minute, datetime.second, datetime.utc_hour_offset > 0 ? "+" : "-",
-                datetime.utc_hour_offset,
-                datetime.utc_minute_offset);
-            return s_datetime;
-        }
 
-        public static foxit.common.DateTime GetLocalDateTime()
-        {
-            DateTimeOffset rime = DateTimeOffset.Now;
-            foxit.common.DateTime datetime = new foxit.common.DateTime();
-            datetime.year = (UInt16)rime.Year;
-            datetime.month = (UInt16)rime.Month;
-            datetime.day = (ushort)rime.Day;
-            datetime.hour = (UInt16)rime.Hour;
-            datetime.minute = (UInt16)rime.Minute;
-            datetime.second = (UInt16)rime.Second;
-
-            datetime.utc_hour_offset = (short)rime.Offset.Hours;
-            datetime.utc_minute_offset = (ushort)rime.Offset.Minutes;
-
-            return datetime;
-        }
+        private static readonly bool is_unix_platform = Environment.OSVersion.Platform == PlatformID.Unix;
 
         public static void SetTableTextStyle(int index, RichTextStyle style)
         {
@@ -66,7 +43,7 @@ namespace TablePDF
                     break;
                 case 4:
                     {
-                        if (Environment.OSVersion.Platform == PlatformID.Unix)
+                        if (is_unix_platform)
                             using (style.font = new Font("Times New Roman", 0, Font.Charset.e_CharsetANSI, 0)) { }
                         else
                             using (style.font = new Font("Times New Roman", 0, Font.Charset.e_CharsetANSI, 0)) { }
@@ -76,7 +53,7 @@ namespace TablePDF
                     }
                 case 5:
                     {
-                        if (Environment.OSVersion.Platform == PlatformID.Unix)
+                        if (is_unix_platform)
                             using (style.font = new Font("FreeSerif", 0, Font.Charset.e_CharsetANSI, 0)) { }
                         else
                             using (style.font = new Font("Times New Roman", 0, Font.Charset.e_CharsetANSI, 0))
@@ -119,11 +96,11 @@ namespace TablePDF
 
         public static string GetTableCellText(int index)
         {
-            string cell_text = " ";
+            string cell_text;
             switch (index)
             {
                 case 0:
-                    cell_text = "Reference style Ifex";
+                    cell_text = "Reference style test";
                     break;
                 case 1:
                     cell_text = "Alignment center";
@@ -136,7 +113,7 @@ namespace TablePDF
                     break;
                 case 4:
                     {
-                        if (Environment.OSVersion.Platform == PlatformID.Unix)
+                        if (is_unix_platform)
                             cell_text = "Red text color, FreeSerif font and alignment right";
                         else
                             cell_text = "Red text color, Times New Roman font and alignment right";
@@ -144,7 +121,7 @@ namespace TablePDF
                     }
                 case 5:
                     {
-                        if (Environment.OSVersion.Platform == PlatformID.Unix)
+                        if (is_unix_platform)
                             cell_text = "Bold, FreeSerif font and alignment right";
                         else
                             cell_text = "Bold, Times New Roman font and alignment right";
@@ -180,20 +157,20 @@ namespace TablePDF
             // Add a spreadsheet with 4 rows and 3 columns
             {
                 int index = 0;
-                using (TableCellDataArray cell_array = new TableCellDataArray())
+                using (TableCellDataArray cell_array = new())
                 {
                     for (int row = 0; row < 4; row++)
                     {
                         // nested using statements
-                        using (RichTextStyle style = new RichTextStyle())
-                        using (TableCellDataColArray col_array = new TableCellDataColArray())
+                        using (RichTextStyle style = new())
+                        using (TableCellDataColArray col_array = new())
                         {
                             for (int col = 0; col < 3; col++)
                             {
                                 string cell_text = GetTableCellText(index);
                                 SetTableTextStyle(index++, style);
-                                Image image = new Image();
-                                using (TableCellData cell_data = new TableCellData(style, cell_text, image, new RectF()))
+                                Image image = new();
+                                using (TableCellData cell_data = new(style, cell_text, image, new RectF()))
                                 {
                                     col_array.Add(cell_data);
                                 }
@@ -203,24 +180,36 @@ namespace TablePDF
                     }
                     float page_width = page.GetWidth();
                     float page_height = page.GetHeight();
-                    TableBorderInfo outside_border_left = new TableBorderInfo();
-                    outside_border_left.line_width = 1;
-                    outside_border_left.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo outside_border_right = new TableBorderInfo();
-                    outside_border_right.line_width = 1;
-                    outside_border_right.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo outside_border_top = new TableBorderInfo();
-                    outside_border_top.line_width = 1;
-                    outside_border_top.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo outside_border_bottom = new TableBorderInfo();
-                    outside_border_bottom.line_width = 1;
-                    outside_border_bottom.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo inside_border_row_info = new TableBorderInfo();
-                    inside_border_row_info.line_width = 1;
-                    inside_border_row_info.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo inside_border_col_info = new TableBorderInfo();
-                    inside_border_col_info.line_width = 1;
-                    inside_border_col_info.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
+                    TableBorderInfo outside_border_left = new()
+                    {
+                        line_width = 1,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo outside_border_right = new()
+                    {
+                        line_width = 1,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo outside_border_top = new()
+                    {
+                        line_width = 1,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo outside_border_bottom = new()
+                    {
+                        line_width = 1,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo inside_border_row_info = new()
+                    {
+                        line_width = 1,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo inside_border_col_info = new()
+                    {
+                        line_width = 1,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
                     using (RectF rect = new RectF(100, 550, page_width - 100, page_height - 100))
                     using (TableData data = new TableData(rect, 4, 3, outside_border_left, outside_border_right, outside_border_top, outside_border_bottom, inside_border_row_info, inside_border_col_info, new TableCellIndexArray(), new FloatArray(), new FloatArray()))
                     {
@@ -232,22 +221,19 @@ namespace TablePDF
             //Add a spreadsheet with 5 rows and 6 columns
             {
                 string cell_text = " ";
-                string[] show_text = { "Foxit Software Incorporated", "Foxit Reader", "Foxit MobilePDF", "Foxit PhantomPDF", "Foxit PDF SDKs" };
-                using (TableCellDataArray cell_array = new TableCellDataArray())
+                string[] show_text = { "Foxit Software Incorporated", "Foxit Reader", "Foxit MobilePDF", "Foxit PhantomPDF", "Foxit PDF SDKs", "Col 6" };
+                using (TableCellDataArray cell_array = new())
                 {
                     for (int row = 0; row < 5; row++)
                     {
-                        using (RichTextStyle style = new RichTextStyle())
-                        using (TableCellDataColArray col_array = new TableCellDataColArray())
+                        using (RichTextStyle style = new())
+                        using (TableCellDataColArray col_array = new())
                         {
                             for (int col = 0; col < 6; col++)
                             {
-                                if (col == 5)
-                                    cell_text = DateTimeToString(GetLocalDateTime());
-                                else
-                                    cell_text = show_text[col];
+                                cell_text = show_text[col];
                                 SetTableTextStyle(row, style);
-                                using (TableCellData cell_data = new TableCellData(style, cell_text, new Image(), new RectF()))
+                                using (TableCellData cell_data = new(style, cell_text, new Image(), new RectF()))
                                 {
                                     col_array.Add(cell_data);
                                 }
@@ -259,26 +245,38 @@ namespace TablePDF
                     float page_width = page.GetWidth();
                     float page_height = page.GetHeight();
 
-                    TableBorderInfo outside_border_left = new TableBorderInfo();
-                    outside_border_left.line_width = 2;
-                    outside_border_left.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo outside_border_right = new TableBorderInfo();
-                    outside_border_right.line_width = 2;
-                    outside_border_right.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo outside_border_top = new TableBorderInfo();
-                    outside_border_top.line_width = 2;
-                    outside_border_top.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo outside_border_bottom = new TableBorderInfo();
-                    outside_border_bottom.line_width = 2;
-                    outside_border_bottom.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo inside_border_row_info = new TableBorderInfo();
-                    inside_border_row_info.line_width = 2;
-                    inside_border_row_info.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    TableBorderInfo inside_border_col_info = new TableBorderInfo();
-                    inside_border_col_info.line_width = 2;
-                    inside_border_col_info.table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid;
-                    using (RectF rect = new RectF(10, 200, page_width - 10, page_height - 350))
-                    using (TableData data = new TableData(rect, 5, 6, outside_border_left, outside_border_right, outside_border_top, outside_border_bottom, inside_border_row_info, inside_border_col_info, new TableCellIndexArray(), new FloatArray(), new FloatArray()))
+                    TableBorderInfo outside_border_left = new()
+                    {
+                        line_width = 2,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo outside_border_right = new()
+                    {
+                        line_width = 2,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo outside_border_top = new()
+                    {
+                        line_width = 2,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo outside_border_bottom = new()
+                    {
+                        line_width = 2,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo inside_border_row_info = new()
+                    {
+                        line_width = 2,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    TableBorderInfo inside_border_col_info = new()
+                    {
+                        line_width = 2,
+                        table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
+                    };
+                    using (RectF rect = new(10, 200, page_width - 10, page_height - 350))
+                    using (TableData data = new(rect, 5, 6, outside_border_left, outside_border_right, outside_border_top, outside_border_bottom, inside_border_row_info, inside_border_col_info, new TableCellIndexArray(), new FloatArray(), new FloatArray()))
                     {
                         TableGenerator.AddTableToPage(page, data, cell_array);
                     }
@@ -287,7 +285,7 @@ namespace TablePDF
         }
         static void Main(string[] args) {
             Env.Load();
-            Console.WriteLine("TablePDF Main");
+            Console.WriteLine("Foxit TABLE PDF");
 
             Directory.CreateDirectory(output_path);
 
@@ -306,7 +304,7 @@ namespace TablePDF
 
             try
             {
-                using (PDFDoc doc = new PDFDoc())
+                using (PDFDoc doc = new())
                 {
                     // Get first page with index 0
                     using (PDFPage page = doc.InsertPage(0, PDFPage.Size.e_SizeLetter))
@@ -315,7 +313,7 @@ namespace TablePDF
                         // Save PDF file
                         string output_file = output_path + "TablePDF.pdf";
                         doc.SaveAs(output_file, (int)PDFDoc.SaveFlags.e_SaveFlagNoOriginal);
-                        Console.WriteLine("electronictable demo.");
+                        Console.WriteLine("Done.");
                     }
                 }
             }
