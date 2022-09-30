90326af6017c28f24415a049a33156b583f3e422
Gideon Idoko <iamgideonidoko@gmail.com>
2022-09-30 23:41:07 +0100

    Attempt implementation completion

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ using foxit.pdf.annots;
 
 using DotNetEnv;
 using ExcelDataReader;
+using System.Data;
+using foxit.pdf.interform;
 
 namespace TablePDF
 {
@@ -18,6 +20,24 @@ namespace TablePDF
         public static readonly string data_path = "./data/";
 
         private static readonly bool is_unix_platform = Environment.OSVersion.Platform == PlatformID.Unix;
+
+        private static int row_count;
+        private static int col_count;
+        private static int num_of_pages;
+        private static readonly int row_per_page = 10;
+        private static readonly int col_per_page = 8;
+        private static DataSet? dataSet;
+
+        public static void GetTables(DataSet dataSet)
+        {
+            // Get Each DataTable in the DataTableCollection and
+            // print each row value.
+            foreach (DataTable table in dataSet.Tables)
+                foreach (DataRow row in table.Rows)
+                    foreach (DataColumn column in table.Columns)
+                        if (row[column] != null)
+                            Console.WriteLine(row[column]);
+        }
 
         public static void SetTableTextStyle(int index, RichTextStyle style)
         {
@@ -98,142 +118,64 @@ namespace TablePDF
             }
         }
 
-        public static string GetTableCellText(int index)
+        public static void AddElectronicTable(PDFPage page, int page_index)
         {
-            string cell_text;
-            switch (index)
-            {
-                case 0:
-                    cell_text = "Reference style God";
-                    break;
-                case 1:
-                    cell_text = "Alignment center";
-                    break;
-                case 2:
-                    cell_text = "Green text color and alignment right";
-                    break;
-                case 3:
-                    cell_text = "Text font size 15";
-                    break;
-                case 4:
-                    {
-                        if (is_unix_platform)
-                            cell_text = "Red text color, FreeSerif font and alignment right";
-                        else
-                            cell_text = "Red text color, Times New Roman font and alignment right";
-                        break;
-                    }
-                case 5:
-                    {
-                        if (is_unix_platform)
-                            cell_text = "Bold, FreeSerif font and alignment right";
-                        else
-                            cell_text = "Bold, Times New Roman font and alignment right";
-                        break;
-                    }
-                case 6:
-                    cell_text = "Bold and italic";
-                    break;
-                case 7:
-                    cell_text = "Bold, italic and alignment center";
-                    break;
-                case 8:
-                    cell_text = "Underline and alignment right";
-                    break;
-                case 9:
-                    cell_text = "Strikethrough";
-                    break;
-                case 10:
-                    cell_text = "CornerMarkSubscript";
-                    break;
-                case 11:
-                    cell_text = "CornerMarkSuperscript";
-                    break;
-                default:
-                    cell_text = " ";
-                    break;
-            }
-            return cell_text;
-        }
-
-        public static void AddElectronicTable(PDFPage page)
-        {
-            // Add a spreadsheet with 4 rows and 3 columns
-            {
-                int index = 0;
-                using TableCellDataArray cell_array = new();
-                for (int row = 0; row < 4; row++)
-                {
-                    // nested using statements
-                    using RichTextStyle style = new();
-                    using TableCellDataColArray col_array = new();
-                    for (int col = 0; col < 3; col++)
-                    {
-                        string cell_text = GetTableCellText(index);
-                        SetTableTextStyle(index++, style);
-                        Image image = new();
-                        using TableCellData cell_data = new(style, cell_text, image, new RectF());
-                        col_array.Add(cell_data);
-                    }
-                    cell_array.Add(col_array);
-                }
-                float page_width = page.GetWidth();
-                float page_height = page.GetHeight();
-                TableBorderInfo outside_border_left = new()
-                {
-                    line_width = 1,
-                    table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
-                };
-                TableBorderInfo outside_border_right = new()
-                {
-                    line_width = 1,
-                    table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
-                };
-                TableBorderInfo outside_border_top = new()
-                {
-                    line_width = 1,
-                    table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
-                };
-                TableBorderInfo outside_border_bottom = new()
-                {
-                    line_width = 1,
-                    table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
-                };
-                TableBorderInfo inside_border_row_info = new()
-                {
-                    line_width = 1,
-                    table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
-                };
-                TableBorderInfo inside_border_col_info = new()
-                {
-                    line_width = 1,
-                    table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
-                };
-                using RectF rect = new(100, 550, page_width - 100, page_height - 100);
-                using TableData data = new(rect, 4, 3, outside_border_left, outside_border_right, outside_border_top, outside_border_bottom, inside_border_row_info, inside_border_col_info, new TableCellIndexArray(), new FloatArray(), new FloatArray());
-                TableGenerator.AddTableToPage(page, data, cell_array);
-            }
-
+           
             //Add a spreadsheet with 5 rows and 6 columns
             {
                 string[] show_text = { "Foxit Software Incorporated", "Foxit Reader", "Foxit MobilePDF", "Foxit PhantomPDF", "Foxit PDF SDKs", "Col 6" };
+                Random rand = new();
                 using TableCellDataArray cell_array = new();
-                for (int row = 0; row < 5; row++)
+
+                DataTable? table = dataSet?.Tables[0];
+                if (dataSet != null)
+                {
+                    GetTables(dataSet);
+                }
+
+                int row_start = 0 * (page_index + 1);
+                int row_end = (page_index + 1) * 10;
+                int actual_row_end = row_end > row_count ? row_count : row_end;
+
+                for (int row = row_start; row < actual_row_end; row++)
                 {
                     using RichTextStyle style = new();
                     using TableCellDataColArray col_array = new();
-                    for (int col = 0; col < 6; col++)
+                    for (int col = 0; col < col_count; col++)
                     {
-                        string cell_text = show_text[col];
+                        DataRow? actual_row = table?.Rows[row];
+                        DataColumn? actual_column = table?.Columns[col];
+                        if (actual_row != null && actual_column != null && actual_row[actual_column] != null)
+                        {
+                            string cell_text = $"{actual_row[actual_column]}";
+                            Console.WriteLine($"cell_text => {actual_row}");
+                            SetTableTextStyle(row, style);
+                            using TableCellData cell_data = new(style, cell_text, new Image(), new RectF());
+                            col_array.Add(cell_data);
+                        }
+                    }
+                    cell_array.Add(col_array);
+                }
+
+                /*for (int row = 0; row < row_count; row++)
+                {
+                    using RichTextStyle style = new();
+                    using TableCellDataColArray col_array = new();
+                    for (int col = 0; col < col_count; col++)
+                    {
+                        string cell_text = show_text[rand.Next(0, show_text.Length)];
                         SetTableTextStyle(row, style);
                         using TableCellData cell_data = new(style, cell_text, new Image(), new RectF());
                         col_array.Add(cell_data);
                     }
                     cell_array.Add(col_array);
-                }
+                }*/
 
                 float page_width = page.GetWidth();
                 float page_height = page.GetHeight();
+
+                Console.WriteLine($"Page width => {page_width}");
+                Console.WriteLine($"Page height => {page_height}");
 
                 TableBorderInfo outside_border_left = new()
                 {
@@ -265,32 +207,40 @@ namespace TablePDF
                     line_width = 2,
                     table_border_style = TableBorderInfo.TableBorderStyle.e_TableBorderStyleSolid
                 };
-                using RectF rect = new(10, 200, page_width - 10, page_height - 350);
-                using TableData data = new(rect, 5, 6, outside_border_left, outside_border_right, outside_border_top, outside_border_bottom, inside_border_row_info, inside_border_col_info, new TableCellIndexArray(), new FloatArray(), new FloatArray());
-                TableGenerator.AddTableToPage(page, data, cell_array);
+                // using RectF rect = new(10, 200, page_width - 10, page_height - 350);
+                using RectF rect = new(10, 200, page_width - 10, page_height - 40);
+                using TableData data = new(rect, row_count, col_count, outside_border_left, outside_border_right, outside_border_top, outside_border_bottom, inside_border_row_info, inside_border_col_info, new TableCellIndexArray(), new FloatArray(), new FloatArray());
+                // TableGenerator.AddTableToPage(page, data, cell_array);
             }
         }
         static void Main(string[] args) {
             try
             {
+                // Add encoding for ExcelReader support
                 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
                 string input_file = data_path + "serious-injury-outcome-indicators-2000-2020.xlsx";
-                Console.WriteLine($"filepath => {input_file}");
 
                 using var stream = File.Open(input_file, FileMode.Open, FileAccess.Read);
 
                 using var reader = ExcelReaderFactory.CreateReader(stream);
+                row_count = reader.RowCount;
+                col_count = reader.FieldCount > col_per_page ? col_per_page : reader.FieldCount;
+                decimal pages = reader.RowCount / row_per_page;
+                num_of_pages = (int) Math.Ceiling(pages);
 
-                Console.WriteLine($"Column count => {reader.FieldCount}, Row count => {reader.RowCount}");
-                // Console.WriteLine($"Data => {reader.GetValue(0)}");
+                dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+                {
+                    UseColumnDataType = false,
+                });
 
-            } catch (Exception ex) when (ex is NotSupportedException)
+                // GetTables(result);
+
+            } catch (Exception ex)
             {
-                Console.WriteLine($"Error => {ex.GetType} : {ex.Message}");
+                Console.WriteLine($"Error: {ex.GetType()} : {ex.Message}");
             }
 
             Env.Load();
-            Console.WriteLine("Foxit TABLE PDF");
 
             Directory.CreateDirectory(output_path);
 
@@ -299,7 +249,7 @@ namespace TablePDF
 
             Console.WriteLine($"sn => {sn}");
 
-            // Initialize library
+            // Initialize Foxit library
             ErrorCode error_code = Library.Initialize(sn, key);
             if (error_code != ErrorCode.e_ErrSuccess)
             {
@@ -310,9 +260,17 @@ namespace TablePDF
             try
             {
                 using PDFDoc doc = new();
-                // Get first page with index 0
-                using PDFPage page = doc.InsertPage(0, PDFPage.Size.e_SizeLetter);
-                AddElectronicTable(page);
+
+                for (int i = 0; i < num_of_pages; i++)
+                {
+                    using PDFPage page = doc.InsertPage(i, PDFPage.Size.e_SizeLetter);
+                    AddElectronicTable(page, i);
+
+                }
+
+                // page2.AddText("TESTING SOME THINGS OUT", new RectF(10, 200, page.GetWidth() - 10, page.GetHeight() - 40), new RichTextStyle());
+
+
                 // Save PDF file
                 string output_file = output_path + "TablePDF.pdf";
                 doc.SaveAs(output_file, (int)PDFDoc.SaveFlags.e_SaveFlagNoOriginal);
